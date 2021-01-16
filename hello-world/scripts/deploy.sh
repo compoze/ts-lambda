b82ac49e095c1dfa5cea98927c024f6c37e7e77a
larse514 <andrew.larse514@gmail.com>
2021-01-15 19:50:58 -0600

    add initial build and deploy

---
+++ hello-world/scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$SCRIPT_DIR/.."
+ROOT_DIR="$PROJECT_DIR/.."
+
+APP_NAME=$1
+
+sam deploy --stack-name ${APP_NAME} \
+    --template "${ROOT_DIR}/template.yaml" \
+    --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-xfrzollpo2o5 \
+    --capabilities CAPABILITY_IAM
