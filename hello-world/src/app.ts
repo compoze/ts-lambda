2ecf9f7cfb864115962a8e203e4a54f00cbe6d48
spawn-dev <spawn-dev>
2021-01-15 19:52:34 -0600

    cleanup

--- hello-world/src/app.ts
+++ hello-world/src/app.ts
@@ -1,11 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-// import * as moment from 'moment'; //  OK
-
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-
-  // const hw: string = process.env.HELLO;
-  // const then: string = moment("2020-12-31").fromNow();
 
   return {
     statusCode: 200,
