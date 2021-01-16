4c49b6ec36d768abf6ec94ae9ae9481dfe11dec4
spawn-dev <spawn-dev>
2021-01-15 22:35:16 -0600

    add deploy no fail on empty

--- hello-world/scripts/deploy.sh
+++ hello-world/scripts/deploy.sh
@@ -11,4 +11,5 @@ APP_NAME=$1
 sam deploy --stack-name ${APP_NAME} \
     --template "${ROOT_DIR}/template.yaml" \
     --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-xfrzollpo2o5 \
-    --capabilities CAPABILITY_IAM
+    --capabilities CAPABILITY_IAM \
+    --no-fail-on-empty-changeset
