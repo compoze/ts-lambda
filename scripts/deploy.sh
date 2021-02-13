96eaec11b70d36629f9b4bd9d27e8d9856ae2f16
spawn-dev <spawn-dev>
2021-02-13 01:36:03 -0600

    revert bucket name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$SCRIPT_DIR/.."
 
 APP_NAME=$1
+ENV=$2
 
-sam deploy --stack-name ${APP_NAME} \
+sam deploy --stack-name ${APP_NAME}-${ENV} \
     --template "${PROJECT_DIR}/template.yaml" \
-    --s3-bucket compozeproduct1-compname-deploymentbucket \
+    --s3-bucket <compoze-deploy-bucket> \
     --capabilities CAPABILITY_IAM \
     --no-fail-on-empty-changeset \
     --region us-east-1
