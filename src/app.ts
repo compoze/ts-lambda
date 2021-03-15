cf72febe81aaf37b63cff7fa93a3d1a4572a2f12
CodyTroopDev <cody.troop@elko.dev>
2021-03-15 16:33:42 -0500

    add logging

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import * as winston from 'winston';
+
+let logger: winston.Logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+      new winston.transports.Console({ level: 'info' }),
+  ],
+});
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info(event);
 
   return {
     statusCode: 200,
