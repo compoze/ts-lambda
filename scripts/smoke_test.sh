ac1299b7fc9df538dfd9078f1b640a0961c72629
spawn-dev <spawn-dev>
2021-07-24 01:48:25 -0500

    add lambda test configuration

---
+++ scripts/smoke_test.sh
@@ -0,0 +1,16 @@
+STACK_NAME=$1
+URL_KEY=$2
+
+URL=$(aws cloudformation describe-stacks --stack-name "$STACK_NAME" --query 'Stacks[0].Outputs[?OutputKey==`'${URL_KEY}'`].OutputValue' --output text)
+echo $URL
+
+RESULT=$(curl -o /dev/null -s -w "%{http_code}\n" ${URL})
+
+echo $RESULT
+
+if [[ "$RESULT" != 200 ]]; then
+    echo "Call failed $RESULT"
+    exit 1
+fi
+
+echo "API is up and working!"
